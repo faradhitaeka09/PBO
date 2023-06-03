655f3d8e5bfe617e3e21b9a58d7a1fdfd28efc36
faradhitaeka09 <129599064+faradhitaeka09@users.noreply.github.com>
2023-06-03 11:04:10 +0700

    Create Kasir.java

---
+++ Tugas Kasir/Kasir.java
@@ -0,0 +1,13 @@
+public class Kasir {
+
+    private int TotalHarga;
+
+    public void setTotalHarga(int totalHarga) {
+        this.TotalHarga += totalHarga;
+    }
+
+    public int getTotalHarga() {
+        return TotalHarga;
+    }
+
+}
